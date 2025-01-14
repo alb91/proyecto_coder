de6fbf267b7dcfd0978778c2a8d75bd648ca4734
alb91 <alb9112@gmail.com>
2025-01-13 21:20:01 -0600

    handlebars & socket.io

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const http = require('http'); 
+const { Server } = require('socket.io'); 
+const { engine } = require('express-handlebars');
+const path = require('path');  
 const productRouter = require('./api/router'); 
 const cartRouter = require('./api/carts');
 
+
+const app = express();
+const server = http.createServer(app); 
+const io = new Server(server); 
 const PORT = 8080; 
 
-app.use(express.json()); 
-app.use('/api/products', productRouter); 
-app.use('/api/carts', cartRouter); 
+// Middleware
+app.use(express.json());
 
-app.listen(PORT, () => {
+// Handlebars
+app.engine('handlebars', engine()); 
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views')); 
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Rutas API 
+app.use('/api/products', productRouter); 
+app.use('/api/carts', cartRouter);
+
+// Ruta Handlebars
+app.get('/', (req, res) => {
+	res.render('home', {title: 'Inicio', message: 'Esto es handlebars'});
+});
+
+io.on('connection', (socket) => {
+	console.log('Nueva conexión');
+
+	socket.emit('message', 'Bienvenido al servidor'); 
+
+	socket.on('disconnect', () => {
+		console.log('Cliente desconectado');
+	})
+})
+
+
+server.listen(PORT, () => {
 	console.log(`Servidor escuchando en http://localhost:${PORT}`);
 }); 
 
